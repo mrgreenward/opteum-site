719eb7146997932d856bf2b7808f8cd1faa975bf
artur <mrgreenward@yandex.ru>
2017-04-10 11:42:15 +0300

    fix menu

--- src/js/components/burger.js
+++ src/js/components/burger.js
@@ -2,6 +2,12 @@ $(document).ready(function () {
     (function () {
         var menuButton = $('.menu-button-js'),
             list = $('.header__menu');
+
+        $(window).resize(function () {
+            if ($(window).width() >= '768'){
+                list.css('display','flex');
+            }
+        });
         menuButton.on('click', function (e) {
             e.preventDefault();
             menuButton.toggleClass('is-active');
